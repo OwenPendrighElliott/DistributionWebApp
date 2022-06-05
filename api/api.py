65b8ead2ad893c25dbf1f7ac223980bf219856a8
jameslindesay <jameslindesay2@gmail.com>
2022-06-05 14:30:44 +1000

    merge conflict?

--- api/api.py
+++ api/api.py
@@ -4,9 +4,20 @@ from flask_cors import CORS
 from flask import request
 import random
 from maffs import get_stats, get_cdf
+import numpy as np
 
 app = Flask(__name__)
 cors = CORS(app)
+
+def set_round_digits(x_min, x_max):
+    n_digit_max = np.floor(np.log10(x_max - x_min)) + 1
+    if n_digit_max == 0:
+        n_digit_round = 5 # This needs to be improved
+    elif n_digit_max < 5:
+        n_digit_round = 5 - n_digit_max
+    else:
+        n_digit_round = 0
+    return int(n_digit_round)
 
 # place holder function for example
 @app.route('/api/calculate_statistics', methods=['POST'])
@@ -27,10 +38,9 @@ def calculate_statistics():
     # n2 = round(random.random(),2)
     # n3 = round(random.random(),2)
 
-    n_digit_round = 2
-
     stats = get_stats(y_coords, get_cdf(y_coords), x_coords, x_min, x_max)
+    n_digit_round = set_round_digits(x_min, x_max)
 
-    return {'mean': f"{stats['mean']: .2f}", 'median': f"{stats['median']: .2f}", 'std': f"{stats['std']: .2f}"}
+    return {'mean': f"{1.123: .{n_digit_round}f}", 'median': f"{stats['median']: .{n_digit_round}f}", 'std': f"{stats['std']: .{n_digit_round}f}"}
     # return {'mean': f"{n1: .2f}", 'median': f"{n2: .2f}", 'std': f"{n3: .2f}"}
 

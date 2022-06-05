e74e8ff37a2ea51eabd435c119cd4017eedde33e
jameslindesay <jameslindesay2@gmail.com>
2022-06-05 14:54:17 +1000

    rounding and dedup

--- api/api.py
+++ api/api.py
@@ -19,6 +19,17 @@ def set_round_digits(x_min, x_max):
         n_digit_round = 0
     return int(n_digit_round)
 
+def deduplicate_vectors(x_vector, y_vector):
+    x_vector, y_vector = x_vector[::-1], y_vector[::-1]
+    i = 0
+    while i < len(x_vector) - 1:
+        j = i + 1
+        while j < len(x_vector) and x_vector[i] == x_vector[j]:
+            x_vector = np.delete(x_vector, j)
+            y_vector = np.delete(y_vector, j)
+        i += 1
+    return x_vector[::-1], y_vector[::-1]
+
 # place holder function for example
 @app.route('/api/calculate_statistics', methods=['POST'])
 def calculate_statistics():
@@ -34,13 +45,10 @@ def calculate_statistics():
     print("X coords:")
     print(x_coords, "\n")
 
-    # n1 = round(random.random(),2)
-    # n2 = round(random.random(),2)
-    # n3 = round(random.random(),2)
-
-    stats = get_stats(y_coords, get_cdf(y_coords), x_coords, x_min, x_max)
+    x_vector, y_vector = deduplicate_vectors(np.array(x_coords), np.array(y_coords))
+    stats = get_stats(y_vector, get_cdf(y_vector), x_vector, x_min, x_max)
     n_digit_round = set_round_digits(x_min, x_max)
 
-    return {'mean': f"{1.123: .{n_digit_round}f}", 'median': f"{stats['median']: .{n_digit_round}f}", 'std': f"{stats['std']: .{n_digit_round}f}"}
+    return {'mean': f"{stats['mean']: .{n_digit_round}f}", 'median': f"{stats['median']: .{n_digit_round}f}", 'std': f"{stats['std']: .{n_digit_round}f}"}
     # return {'mean': f"{n1: .2f}", 'median': f"{n2: .2f}", 'std': f"{n3: .2f}"}
 

d894eb1c939526384e7455fee60800f3bddb5917
jameslindesay <jameslindesay2@gmail.com>
2022-06-06 20:40:58 +1000

    Fixed mean, progress on std, small optimisations

--- api/api.py
+++ api/api.py
@@ -2,8 +2,8 @@ import time
 from flask import Flask
 from flask_cors import CORS
 from flask import request
-import random
-from maffs import get_stats, get_cdf
+from maffs import get_stats
+from utils import prep_input_vectors
 import numpy as np
 
 app = Flask(__name__)
@@ -12,29 +12,13 @@ cors = CORS(app)
 def set_round_digits(x_min, x_max):
     n_digit_max = np.floor(np.log10(x_max - x_min)) + 1
     if n_digit_max == 0:
-        n_digit_round = 5 # This needs to be improved
+        n_digit_round = 5 # This could be improved
     elif n_digit_max < 5:
         n_digit_round = 5 - n_digit_max
     else:
         n_digit_round = 0
     return int(n_digit_round)
 
-def deduplicate_vectors(x_vector, y_vector):
-    x_vector, y_vector = x_vector[::-1], y_vector[::-1]
-    i = 0
-    while i < len(x_vector) - 1:
-        j = i + 1
-        while j < len(x_vector) and x_vector[i] == x_vector[j]:
-            x_vector = np.delete(x_vector, j)
-            y_vector = np.delete(y_vector, j)
-        i += 1
-    return x_vector[::-1], y_vector[::-1]
-
-def shift_x_vector(x_vector, x_min, x_max):
-    x_vector_min, x_vector_max = np.min(x_vector), np.max(x_vector)
-    return (x_vector - x_vector_min) / (x_vector_max - x_vector_min) * (x_max - x_min) + x_min
-
-# place holder function for example
 @app.route('/api/calculate_statistics', methods=['POST'])
 def calculate_statistics():
     data = request.get_json()
@@ -44,16 +28,18 @@ def calculate_statistics():
     x_min = data['xMin']
     x_max = data['xMax']
 
-    print("Y coords:")
-    print(y_coords, "\n")
-    print("X coords:")
-    print(x_coords, "\n")
+    # print("Y coords:")
+    # print(y_coords, "\n")
+    # print("X coords:")
+    # print(x_coords, "\n")
 
-    x_vector, y_vector = deduplicate_vectors(np.array(x_coords), np.array(y_coords))
-    x_vector = shift_x_vector(x_vector, x_min, x_max)
-    stats = get_stats(y_vector, get_cdf(y_vector), x_vector, x_min, x_max)
+    x_vector, y_vector = prep_input_vectors(x_coords, y_coords, x_min, x_max)
+
+    print(x_vector, "\n")
+    print(y_vector, "\n")
+
+    stats = get_stats(x_vector, y_vector)
     n_digit_round = set_round_digits(x_min, x_max)
 
     return {'mean': f"{stats['mean']: .{n_digit_round}f}", 'median': f"{stats['median']: .{n_digit_round}f}", 'std': f"{stats['std']: .{n_digit_round}f}"}
-    # return {'mean': f"{n1: .2f}", 'median': f"{n2: .2f}", 'std': f"{n3: .2f}"}
 
--- api/maffs.py
+++ api/maffs.py
@@ -1,10 +1,20 @@
 import numpy as np
 
-def initialise_test_matrix(length):
-    #return np.random.randint(0, np.random.randint(100,1000), length)
+def initialise_test_matrix():
     #return [1, 1.1, 1.2, 1.6, 2.3, 2.9, 3.9, 7, 7.8, 8.2, 8.3, 8.2, 7.8, 7, 3.9, 2.9, 2.3, 1.6, 1.2, 1.1, 1]
     #return  [0, 1, 0]
     return np.array([1.4867195147343e-06, 2.45106104294233e-06, 3.99989037241663e-06, 6.4611663926488e-06, 1.03310065813217e-05, 1.63509588958253e-05, 2.56160811951383e-05, 3.97238223814148e-05, 6.09759039529897e-05, 9.26476353230142e-05, 0.000139341123134969, 0.000207440308767921, 0.000305686225478055, 0.000445889724807599, 0.000643795497926865, 0.000920104769622966, 0.00130165384164891, 0.00182273109600128, 0.00252649577810393, 0.00346643791897576, 0.00470779076312334, 0.00632877642858276, 0.00842153448411835, 0.0110925548393749, 0.0144624147976342, 0.0186646099340664, 0.0238432745020429, 0.0301496139168006, 0.0377369231406399, 0.0467541424067055, 0.0573380051248129, 0.0696039583923258, 0.0836361772145172, 0.0994771387927487, 0.117117359532744, 0.13648600918747, 0.157443187618844, 0.179774665124813, 0.203189835501224, 0.227323505631361, 0.251741946984239, 0.275953371470115, 0.2994226832711, 0.32159002340941, 0.341892294166129, 0.359786557812623, 0.37477397940639, 0.386422853089569, 0.394389234004919, 0.398433801691346, 0.398433801691346, 0.394389234004919, 0.386422853089569, 0.37477397940639, 0.359786557812623, 0.341892294166129, 0.32159002340941, 0.2994226832711, 0.275953371470116, 0.251741946984239, 0.227323505631361, 0.203189835501224, 0.179774665124813, 0.157443187618844, 0.13648600918747, 0.117117359532744, 0.0994771387927487, 0.0836361772145173, 0.0696039583923258, 0.0573380051248129, 0.0467541424067055, 0.03773692314064, 0.0301496139168007, 0.0238432745020429, 0.0186646099340664, 0.0144624147976342, 0.0110925548393749, 0.00842153448411835, 0.00632877642858276, 0.00470779076312334, 0.00346643791897576, 0.00252649577810393, 0.00182273109600128, 0.00130165384164891, 0.000920104769622967, 0.000643795497926863, 0.000445889724807599, 0.000305686225478057, 0.00020744030876792, 0.000139341123134969, 9.26476353230145e-05, 6.09759039529897e-05, 3.9723822381415e-05, 2.56160811951381e-05, 1.63509588958253e-05, 1.03310065813217e-05, 6.4611663926488e-06, 3.99989037241663e-06, 2.45106104294232e-06, 1.4867195147343e-06])
+
+def deduplicate_vectors(x_vector, y_vector):
+    x_vector, y_vector = x_vector[::-1], y_vector[::-1]
+    i = 0
+    while i < len(x_vector) - 1:
+        j = i + 1
+        while j < len(x_vector) and x_vector[i] == x_vector[j]:
+            x_vector = np.delete(x_vector, j)
+            y_vector = np.delete(y_vector, j)
+        i += 1
+    return x_vector[::-1], y_vector[::-1]
 
 def auc(y_vector: np.ndarray) -> float:
     '''
@@ -20,8 +30,9 @@ def auc(y_vector: np.ndarray) -> float:
         >>> area = auc(y_vec)
     '''
     area = 0
-    for i in range(len(y_vector) - 1):
-        area += (y_vector[i+1] + y_vector[i]) / 2 / (len(y_vector) - 1)
+    count_y = len(y_vector)
+    for i in range(count_y - 1):
+        area += (y_vector[i+1] + y_vector[i]) / 2 / (count_y - 1)
     return area
 
 def normalise_input_y_vector(y_vector: np.ndarray) -> np.ndarray:
@@ -52,30 +63,47 @@ def get_cdf(y_vector: np.ndarray) -> np.ndarray:
         >>> y_vec = np.array([1, 2, 3, 4, 5])
         >>> cdf = get_cdf(y_vec)
     '''
-    list_area = np.zeros(len(y_vector))
+    count_y = len(y_vector)
+    list_area = np.zeros(count_y)
     cum_area = 0
-    for i in range(len(y_vector) - 1):
-        cum_area += (y_vector[i+1] + y_vector[i]) / 2 / (len(y_vector) - 1)
+    for i in range(count_y - 1):
+        cum_area += (y_vector[i+1] + y_vector[i]) / 2 / (count_y - 1)
         list_area[i] = cum_area
     return ([0] + list_area) / np.max(list_area)
 
-# Not correct
-def get_mean(y_vector, x_vector, min_x, max_x):
+def interp_x_y_vectors(x_vector, y_vector):
+    '''
+    Interpolates a y vector from x integers between the input minimum and maximum x. Requires the x vector to be integers.
+
+    Args:
+        x_vector: The x vector to interpolate from.
+        y_vector: The y vector to interpolate to.
+    Returns:
+        np.ndarray: The interpolated y vector.
+
+    Examples:
+        >>> x_vector = np.array([1, 3, 5])
+        >>> y_vector = np.array([2, 6, 10])
+        >>> interp_x_y_vectors(x_vector, y_vector) = np.array([1, 2, 3, 4, 5]), np.array([2, 4, 6, 8, 10])
+    '''
+    new_x_vector = np.arange(np.min(x_vector), np.max(x_vector) + 1)
+    new_y_vector = np.interp(new_x_vector, x_vector, y_vector)
+    return new_x_vector, new_y_vector
+
+def get_mean(x_vector, y_vector, x_range):
     mean = 0
     for i in range(len(y_vector) - 1):
-        mean += (x_vector[i+1] + x_vector[i]) * (y_vector[i+1] + y_vector[i]) / 4 / (x_vector[i+1] - x_vector[i])
+        mean += (x_vector[i+1] + x_vector[i]) / 2 * (y_vector[i+1] + y_vector[i]) / 2 * (x_vector[i+1] - x_vector[i]) / x_range
     return mean
 
-# Not correct
-def get_median(cdf, x_vector):
+def get_median(x_vector, cdf):
     return x_vector[np.argmin(np.abs(cdf - 0.5))]
 
 # Not correct
-def get_std(matrix, x, min_x, max_x):
+def get_std(x_vector, y_vector, mean, x_range):
     std = 0
-    mean = get_mean(matrix, x, min_x, max_x)
-    for i in range(len(matrix) - 1):
-        std += ((((x[i+1] + x[i]) / 2 - mean) ** 2) * (matrix[i+1] + matrix[i]) / (2 ** (max_x + 1)) * 1/0.12445531191556146 / (len(matrix) - 1) * (max_x - min_x) / max_x) ** 0.5
+    for i in range(len(y_vector) - 1):
+        std += ((((x_vector[i+1] + x_vector[i]) / 2 - mean) ** 2) * (y_vector[i+1] + y_vector[i]) / 2 * (x_vector[i+1] - x_vector[i]) / x_range) ** 0.5
     return std
 
 def plot(x_vector, y_vector):
@@ -84,41 +112,28 @@ def plot(x_vector, y_vector):
     plt.show()
 
 def get_stats(
-              y_vector: np.ndarray, 
-              cdf: np.ndarray, 
-              x_vector: np.ndarray, 
-              min_x: float, 
-              max_x: float
+              x_vector: np.ndarray,
+              y_vector: np.ndarray,
               ) -> dict:
     '''
     Given a y vector, an x vector and a CDF use a provided x vector range to 
     extract the mean, median and standard deviation of the distribution that they represent.
 
     Args:
-        y_vector: The y vector to get the stats of.
-        cdf: The CDF of the y vector.
-        x_vector: The x vector of the y vector.
-        min_x: The minimum x value of the x vector.
-        max_x: The maximum x value of the x vector.
+        x_vector: The vector of x points.
+        y_vector: The vector of y points.
     Returns:
-        tuple: The mean, median and standard deviation of the distribution that the y vector represents.
-
+        tuple: The mean, median and standard deviation of the distribution that the x and y vectors represent.
     '''
+    x_max = np.max(x_vector)
+    x_min = np.min(x_vector)
+    x_range = x_max - x_min
+    cdf = get_cdf(y_vector)
+    mean = get_mean(x_vector, y_vector, x_range)
+    median = get_median(x_vector, cdf)
+    std = get_std(x_vector, y_vector, mean, x_range)
     return {
-            'mean': get_mean(y_vector, x_vector, min_x, max_x), 
-            'median': get_median(cdf, x_vector), 
-            'std': 3#get_std(matrix, x, min_x, max_x)
+            'mean': mean,
+            'median': median,
+            'std': std
            }
-
-def main():
-    min_x, max_x = 0, 100
-    y = normalise_input_y_vector(initialise_test_matrix(10000))
-    x = np.linspace(min_x, max_x, len(y))
-    print(x)
-    print(get_stats(y, get_cdf(y), x, min_x, max_x))
-    # print(x)
-    # print(y)
-    # plot(x, y)
-
-if __name__ == "__main__":
-    main()

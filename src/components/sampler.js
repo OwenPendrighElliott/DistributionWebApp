55c527733a0f8f839f325cfd8d073068ab89ad8d
OwenPendrighElliott <owenpendrighelliott@gmail.com>
2022-07-11 21:25:48 +1000

    whoops wrong branch

--- src/components/sampler.js
+++ src/components/sampler.js
@@ -33,12 +33,54 @@ const Sampler = () => {
     const [samplePoints, setSamplePoints] = useState([])
     const [open, setOpen] = React.useState(false);
 
+    const options = {
+        chart: {
+            title: "Sample Histogram",
+        },
+    };
+
     const { xCoordinates, yCoordinates, xMin, xMax, nSamples, setNSamples } = useContext(DistributionContext)
+
+    function arrToBins(array, nBins) {
+    
+        let min = Math.min(...array);
+        let max = Math.max(...array);
+        let inc = (max-min) / nBins;
+    
+        let plotData = [["Range", "nSamples"]];
+    
+        let prev = min;
+    
+        for (let i = 0; i < nBins; i++) {
+            let count = 0;
+            for (let j = 0; j < array.length; j++){
+                if (array[j] > prev && array[j] < prev+inc) {
+                    count = count + 1;
+                }
+            }
+            let minBound = Math.round(prev);
+            let maxBound = Math.round(prev+inc);
+            if (i==0) {
+                minBound = xMin;
+            }
+            if (i==nBins-1){
+                maxBound = xMax;
+            }
+            let bucket = minBound + " - " + maxBound;
+            plotData.push([bucket, count]);
+            prev = prev + inc;
+        }
+    
+        return plotData;
+    }
 
     function callSampleAPI() {
         let result = getSamples(xCoordinates, yCoordinates, Number(xMin), Number(xMax), Number(nSamples));
         setSamplePoints(result);
         copyArrayToClipboard(result);
+
+
+        console.log(arrToBins(result, 10));
     }
 
     const handleTooltipClose = () => {

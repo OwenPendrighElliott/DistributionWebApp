ce6eb840228c8cac0d3d9ebd3b358bbdfa8a4843
OwenPendrighElliott <owenpendrighelliott@gmail.com>
2022-07-11 18:02:11 +1000

    remove gradient

--- src/components/rangeSelector.js
+++ src/components/rangeSelector.js
@@ -8,15 +8,27 @@ import Grid from '@mui/material/Grid';
 import { MinValidationTextField, MaxValidationTextField } from './validIntTextFields';
 
 const RangeSelector = (width) => {
-    const {xMin, xMax} = useContext(DistributionContext)
+    const {xMin, xMax, setXMax, setXMin} = useContext(DistributionContext)
 
     return (
         <Grid container sx={{ maxWidth: width }}>
             <Grid item xs={true} style={{ display: "flex", justifyContent: "left" }}>
-                <MaxValidationTextField  label={"Lower Bound"} defaultValue={xMin} maxInt={xMax}></MaxValidationTextField>
+                {/* <MaxValidationTextField  label={"Lower Bound"} defaultValue={xMin} maxInt={xMax}></MaxValidationTextField> */}
+                <TextField
+                    label='Left Bound'
+                    type={'number'}
+                    onChange={(e) => setXMin(e.target.value)}
+                    value={xMin}
+                />
             </Grid>
             <Grid item xs={true} style={{ display: "flex", justifyContent: "right"}}>
-                <MinValidationTextField  label={"Upper Bound"} defaultValue={xMax} minInt={xMin}></MinValidationTextField>
+                {/* <MinValidationTextField  label={"Upper Bound"} defaultValue={xMax} minInt={xMin}></MinValidationTextField> */}
+                <TextField
+                    label='Right Bound'
+                    type={'number'}
+                    onChange={(e) => setXMax(e.target.value)}
+                    value={xMax}
+                />
             </Grid>
         </Grid>
     )
